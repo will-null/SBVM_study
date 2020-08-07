6d3a9d84564f0a0cf1b471cd9bf2a7f949493d21
will-null <suiai1994@163.com>
2020-08-07 17:50:41 +0800

    alden

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,11 +1,16 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class LoginController {
 
-
-
-
-
+    @RequestMapping("/he")
+    public String aappp() {
+        return "index";
+    }
 
 
 }
